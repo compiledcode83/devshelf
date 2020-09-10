59f16b3dff33b7d2a215c63af84c6ee78581b90d
residentevil0803 <ramseyklassen0803@gmail.com>
2020-09-10 18:09:54 +0200

    Add and setup body-parser and cors

--- packages/rate-my-app-backend/src/index.ts
+++ packages/rate-my-app-backend/src/index.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
 
 const app = express();
-const PORT = 5000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
 
 app.use('/', (_, res) => {
   res.status(200).send({ data: 'Hello world!' });
