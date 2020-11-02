990604a01455f32cb89d2d2ab5c0fde2715d11b2
residentevil0803 <ramseyklassen0803@gmail.com>
2020-11-02 17:43:20 +0100

    Create main router file

---
+++ packages/api/src/router/router.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+import { passportRouter } from './passportRouter';
+import { sessionRouter } from './sessionRouter';
+
+export const router = Router();
+router.use('/auth', passportRouter);
+router.use('/session', sessionRouter);
