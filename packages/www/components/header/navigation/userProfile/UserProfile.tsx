69465b573930ecb55ed55f353ed59db084b3b7e0
residentevil0803 <ramseyklassen0803@gmail.com>
2020-11-02 19:52:31 +0100

    feat(component): replace normal image with Next Image component

--- packages/www/components/header/navigation/userProfile/UserProfile.tsx
+++ packages/www/components/header/navigation/userProfile/UserProfile.tsx
@@ -1,8 +1,9 @@
+import Image from 'next/image';
 import styles from './userProfile.module.scss';
 
 const UserProfile = () => (
   <div className={styles.wrapper}>
-    <img src="/images/user.png" className={styles.image} alt="" />
+    <Image unsized src="/images/user.png" className={styles.image} alt="" />
     <p className={styles.text}>
       <span className="visually-hidden">Profil użytkownika </span>
       Olaf Sulich
