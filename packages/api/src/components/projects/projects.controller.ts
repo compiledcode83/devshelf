9e10fdce23706bf7c70c96232d58a8c5aabd6c89
residentevil0803 <ramseyklassen0803@gmail.com>
2020-11-09 23:57:26 +0100

    feat(api): add project service findOne

--- packages/api/src/components/projects/projects.controller.ts
+++ packages/api/src/components/projects/projects.controller.ts
@@ -1,4 +1,4 @@
-import { findMany } from './projects.service';
+import { findMany, findOne } from './projects.service';
 import { Router } from 'express';
 
 export const projectsRouter = Router();
@@ -6,4 +6,10 @@ export const projectsRouter = Router();
 projectsRouter.get('/', async (_req, res) => {
   const projects = await findMany();
   return res.status(200).json({ data: projects });
+});
+
+projectsRouter.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const project = await findOne(id);
+  return res.status(200).json({ data: project });
 });
--- packages/api/src/components/projects/projects.service.ts
+++ packages/api/src/components/projects/projects.service.ts
@@ -3,5 +3,17 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const findMany = async () => {
-  return await prisma.project.findMany();
+  return await prisma.project.findMany({
+    include: {
+      author: true,
+    },
+  });
+};
+
+export const findOne = async (id: string) => {
+  return await prisma.project.findOne({
+    where: {
+      id: Number(id),
+    },
+  });
 };
