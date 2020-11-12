ba7ececde4240eb97151c21dc75c744b644cc7f7
residentevil0803 <ramseyklassen0803@gmail.com>
2020-11-12 13:41:02 +0100

    feat(api): improve types for getEnvVariable function

--- packages/api/src/utils/getEnvVariable.ts
+++ packages/api/src/utils/getEnvVariable.ts
@@ -1,8 +1,19 @@
-export const getEnvVariable = (name: string): string => {
-  const val = process.env[name];
-  if (val !== undefined) {
-    return val;
+type Names = Readonly<{
+  GITHUB_ID: string;
+  GITHUB_SECRET: string;
+  GITHUB_CALLBACK_URL: string;
+  SESSION_SECRET: string;
+  ORIGIN: string;
+  GOOGLE_CLIENT_ID: string;
+  GOOGLE_CLIENT_SECRET: string;
+  GOOGLE_CALLBACK_URL: string;
+}>;
+
+export const getEnvVariable = <T extends keyof Names>(name: T): Names[T] => {
+  const value = process.env[name];
+  if (value !== undefined) {
+    return value;
   } else {
-    throw new Error(`Wrong process.env[${name}]`);
+    throw new Error(`Cannot find environmental variable: ${name}`);
   }
 };
