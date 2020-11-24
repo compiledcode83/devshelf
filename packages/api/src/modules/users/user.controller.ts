cf6b45624c141e4e1b6d3c08a8772f6585173fd5
residentevil0803 <ramseyklassen0803@gmail.com>
2020-11-24 23:10:34 +0100

    feat(api): create delete user endpoint

--- packages/api/src/modules/users/user.controller.ts
+++ packages/api/src/modules/users/user.controller.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getUsers, findOne, getFeedback, getProjects } from './users.service';
+import { getUsers, findOne, getFeedback, getProjects, deleteUser } from './users.service';
 
 export const usersRouter = Router();
 
@@ -24,4 +24,10 @@ usersRouter.get('/:id/projects', async (req, res) => {
   const userId = req.params.id;
   const projects = await getProjects(userId);
   return res.status(200).json({ projects });
+});
+
+usersRouter.delete('/:id', async (req, res) => {
+  const userId = req.params.id;
+  const userToDelete = deleteUser(userId);
+  return res.status(200).json({ userToDelete });
 });
