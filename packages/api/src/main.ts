8d9e2356338ff92a87f9d9c85d3fe982493c8116
residentevil0803 <ramseyklassen0803@gmail.com>
2021-01-27 19:41:43 +0100

    feat(api): create getConfig function

--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,9 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as cookieParser from 'cookie-parser';
+import * as session from 'express-session';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
+import { getConfig } from './utils/getConfig';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
+  app.use(helmet());
+  app.use(
+    session({
+      secret: getConfig('SESSION_SECRET'),
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+
   const swaggerConfig = new DocumentBuilder()
     .setTitle('DevBooks')
     .setDescription('DevBooks API description')
